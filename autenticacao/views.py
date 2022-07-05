4f5bc648775e1a8ba409eadd73ca54fab94c3539
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-05 00:01:33 -0300

    feat: Cria arquivo html para tela de cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse("Você está na página de cadastro")
+    return render(request, 'cadastro.html')
 
 def logar(request):
     return HttpResponse("Você está na página de login")
