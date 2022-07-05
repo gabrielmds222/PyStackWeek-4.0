db1caae9f720e8d3b2cd0cc340c066702bdefd73
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-07-04 23:57:23 -0300

    feat: Cria url para página de login

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def cadastro(request):
     return HttpResponse("Você está na página de cadastro")
+
+def logar(request):
+    return HttpResponse("Você está na página de login")
